988018c59830ecc6bbbf8d6afaf25b3072ae7b4b
Hruthvika Babburi <hruthvikababburi@gmail.com>
2024-06-03 21:21:06 +0530

    added responsiveness and wroking on the functionality.

--- script.js
+++ script.js
@@ -1,15 +1,56 @@
 const searchBtn = document.querySelector(".search-btn");
-const cityInput = document.querySelector(".city-input")
-const locationBtn = document.querySelector(".location-search-btn");
+const cityInput = document.querySelector(".city-input");
+const API_KEY = "3065705f5a9cdacd92c9c9b1c5542cf9";
 
-const getCityName=()=>{
-    const cityName = cityInput.value.trim()
-    if(cityName ==""){
-        alert('Enter Valid City')
-    }
-    console.log(cityName);
-    cityInput.value="";
-    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`
+const getWeatherDetails = (cityName,lat,lon)=>{
+    const WEATHER_API_URL = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    fetch(WEATHER_API_URL).then(res=>res.json()).then(data=>{
+        const uniqueForecastDays  = []
+        const sixDaysForecast = data.list.filter(forecast=>{
+            const forecastDate = new Date(forecast.dt_txt).getDate()
+            if(!uniqueForecastDays.includes(forecastDate)){
+                return uniqueForecastDays.push(forecastDate)
+            }
+        })
+        console.log(sixDaysForecast)
+    }).catch((error) => {
+        console.error('Fetch error:', error);
+        alert('An error has occurred while fetching the weather forecast!');
+    });
 }
 
-searchBtn.addEventListener('click',getCityName)
+
+const getCityName = () => {
+    const cityName = cityInput.value.trim();
+    if (cityName === "") {
+        alert('Enter Valid City');
+        return;
+    }
+
+    console.log(cityName);
+    cityInput.value = "";
+
+    const GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}`;
+
+    fetch(GEOCODING_API_URL)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            if (data.length === 0) {
+                alert('City not found');
+            }
+            const {name, lat, lon} = data[0];
+            getWeatherDetails(name,lat,lon);
+        })
+        .catch((error) => {
+            console.error('Fetch error:', error);
+            alert('An error has occurred while fetching the details');
+        });
+};
+
+searchBtn.addEventListener('click', getCityName);
